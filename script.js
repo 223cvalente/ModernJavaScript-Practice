12cff3f769dd9521e4fa1a0d16e74ab6cf5c96c5
ChristopherV3 <84456226+223cvalente@users.noreply.github.com>
2022-10-03 13:28:17 +0000

    almost finished the 3rd video

--- script.js
+++ script.js
@@ -151,12 +151,52 @@
 //}
 
                               //while loops
-let i = 0;
-while( i < 5){
-  console.log('in loop: ', i);
- i++;
+const names = ['shaun', 'mario', 'luigi'];
+//let i = 0;
+//while( i < 5){
+//  console.log('in loop: ', i);
+// i++;
+//}
+
+//let i = 0;
+//while(i < names.length){
+// console.log(names[i]);
+//  i++;
+//}
+                                 //while loops
+//let i = 5;
+//do(
+ // console.log('val of i is: ', i);
+ // i++;
+//} while(i < 5);
+
+                                // if statements
+
+const age = 25;
+
+if(age > 20 ){
+  console.log('you are over 20 years old');
 }
 
+const ninjas = ['shaun','ryu', 'chun-li', 'yoshi'];
 
+if(ninjas.length > 3){
+  console.log("that's a lot of ninjas");
+}
+                            //logical operators 
+const password = 'password1234';
+if(password.length >= 12 && password.includes('@')){
+  console.log('that password is mighty strong');
+}else if(password.length >= 8 || password.includes('@')){
+  console.log('that password is strong enough! ');
+}else{
+   console.log('that password is not strong enough! ');
+}
 
+                          //logical NOT 
 
+if(condition){
+  
+}
+
+//33min left    
