7e62b2c0a2d538dbde8942f680ddf155beedde3c
ChristopherV3 <84456226+223cvalente@users.noreply.github.com>
2022-10-03 13:03:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,162 @@
+                    //common string methods
+
+//let email ='mario@thenetninja.co.uk'
+
+//let result = email.lastIndexOf('n');
+
+//let result = email.slice(2,5);
+
+
+// let result = email.substr(4,10);
+
+
+// let result= email.replace('m', 'w');
+
+// let result  = email.replace('n'. 'w');
+
+
+//let radius = 10; 
+//const pi= 3.14;
+
+//console.log (radius, pi);
+
+                  //math operatirs +. -. * / ** %
+
+//console.log(10 / 2);
+//let result = radius % 3;
+
+//let result = pi * radius **2;
+
+
+
+//console.log(result);
+
+//let likes = 10;
+//likes= likes + 1;
+//likes++;
+//likes+=10 ;
+
+
+//console.log(likes);
+
+
+//let result = ' the blog has ' + likes + 'likes'; 
+//console.log(result);
+
+
+//const title = 'Best reads of 2019';
+//const author = 'Mario';
+//const likes = 30;
+
+                  //concatenation way
+
+//let result = ' the blog called' + title + 'by ' + author ' has ' + likes + 'likes';
+//console.log(result);
+
+                    //template way 
+//let result = `the blog called ${title} by ${author} has ${likes} likes`;
+//console.log(result);
+
+//creating html templates
+
+//let html = `
+//<h2>${title}</h2>
+//<p> By ${author}</p>
+//<span> this blog has ${likes} likes</span>
+
+//`;
+
+//console.log(html);
+
+
+
+//let ninjas = ['Shaun', 'ryu', 'chun-lit'];
+//ninjas[1] = 'ken';
+//console.log(ninjas[1]);
+
+//let ages = [20,25,30,45];
+//console.log(ages[2]);
+
+//let random = ['shaun', 'crystal', 30, 20];
+//console.log(ninjas.length);
+
+//array methods
+
+//let result = ninjas.join('-');
+
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken', 'crystal']);
+//let result = ninjas.push('ken');
+// result = ninjas.pop();
+//console.log(result);
+
+
+
+//let age = null; 
+//console.log(age, age + 3, 'the age is ${age}');
+
+
+                        //booleans and comparisons
+
+//console.log(true, false, "true", "false");
+
+//methods can return booleans
+//let email = 'liugi@theninja.co.uk';
+//let names = ['mario', 'luigi', 'toad'];
+
+//let result = email.includes('!');
+//let result = names.includes('luigi');
+//console.log(result);
+
+                          //comparsion operators
+
+//let age = 25;
+//console.log(age == 25);
+//console.log(age == 30);
+//console.log(age != 30);
+//console.log(age < 20);
+
+//let name = 'shaun';
+//console.log(name == 'shaun');
+
+                              //type conversion
+//let score  = '100';
+
+//score = Number(score);
+//console.log(score + 1 );
+
+//let result = String(50); 
+
+//let result = Boolean(100);
+
+//let result =  Boolean('0');  //makes it true or false
+//console.log(result, typeof result);
+
+
+                                  // for loops 
+
+//for(let i = 0; i < 5; i++){
+//  console.log('in loop:', i);
+  
+//}
+
+//console.log('loop finished');
+
+//const names = ['shaun', 'mario', 'luigi'];
+
+//for( let i = 0; i < names.length; i++){
+  //console.log(names[i]);
+ // let html = `<div>${names[i]} </div>`;
+  //console.log(html);
+//}
+
+                              //while loops
+let i = 0;
+while( i < 5){
+  console.log('in loop: ', i);
+ i++;
+}
+
+
+
+
